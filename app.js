d239f9f5a270f1c5cf1d973d1a9a3f2001b30ae9
favourflav <chukwagon1@gmail.com>
2023-07-22 02:38:02 -0700

    only added local host url and prodcution url for cors

--- app.js
+++ app.js
@@ -13,7 +13,19 @@ const app = express();
 dotenv.config();
 app.use(helmet())
 app.use(morgan("dev"));
-app.use(cors());
+// cors options
+const whiteList = [`${process.env.PRODUCTION_URL}`,`${process.env.LOCALHOST_URL}`]
+
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (whiteList .indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
